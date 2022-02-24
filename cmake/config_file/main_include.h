fec032b2d3fe5d87d2963ba21b02b7069446e829
Christian Heimlich <chris@pcserenity.com>
2022-02-24 03:22:01 -0500

    Make main header generator unique, include build info in main header
    Also simplify platform detection

---
+++ cmake/config_file/main_include.h
@@ -0,0 +1,12 @@
+/* [${LIB_NAME}]
+ * Edition: ${LIB_EDITION}
+ * Version: ${LIB_VER}
+ * Qt Version: ${QT_VER}
+ * Platform: ${PLATFORM}
+ * Build Date: ${BUILD_DATE}
+*/
+#ifndef ${MAIN_INCLUDE_NAME}
+#define ${MAIN_INCLUDE_NAME}
+
+${MODULE_INCLUDE_FILES}
+#endif // ${MAIN_INCLUDE_NAME}
