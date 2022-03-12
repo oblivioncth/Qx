dce78081d2bad5d14271855f075e44e5bc00789d
Christian Heimlich <chris@pcserenity.com>
2022-03-12 05:21:33 -0500

    Start tracking Doxygen

---
+++ doc/snippets/qx/core/bitarray.cpp
@@ -0,0 +1,19 @@
+//! [0]
+Qx::BitArray ba = Qx::BitArray::fromInteger(quint16(20));
+ba.at(0); // 0 
+ba.at(1); // 0
+ba.at(2); // 1
+ba.at(3); // 0
+ba.at(4); // 1
+ba.at(5); // 0
+ba.at(6); // 0
+ba.at(7); // 0
+ba.at(8); // 0
+ba.at(9); // 0
+ba.at(10); // 0
+ba.at(11); // 0
+ba.at(12); // 0
+ba.at(13); // 0
+ba.at(14); // 0
+ba.at(15); // 0
+//! [0]
