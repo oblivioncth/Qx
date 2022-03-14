011960da21d468d218220267a0e3d34a70154456
Christian Heimlich <chris@pcserenity.com>
2022-03-13 22:27:09 -0400

    Add basic documentation of qx-integrity

--- comp/core/src/qx-integrity.cpp
+++ comp/core/src/qx-integrity.cpp
@@ -8,8 +8,18 @@ namespace Qx
 // Integrity
 //===============================================================================================================
 
+/*!
+ *  @class Integrity
+ *
+ *  @brief The Integrity class is a collection of static functions pertaining to data completeness
+ */
+
 //-Class Functions---------------------------------------------------------------------------------------------
 //Public:
+
+/*!
+ *  Computes the checksum of @a data using @a hashAlgorithm and returns it as a hexadecimal string.
+ */
 QString Integrity::generateChecksum(QByteArray& data, QCryptographicHash::Algorithm hashAlgorithm)
 {
     QCryptographicHash checksumHash(hashAlgorithm);
