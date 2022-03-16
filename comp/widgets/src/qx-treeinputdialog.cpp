92b673a52f346dbc1be75c523fea758b033471c5
Christian Heimlich <chris@pcserenity.com>
2022-03-16 14:03:24 -0400

    Add context parameter to TreeInputDialog lambda connections

--- comp/widgets/src/qx-treeinputdialog.cpp
+++ comp/widgets/src/qx-treeinputdialog.cpp
@@ -26,8 +26,8 @@ TreeInputDialog::TreeInputDialog(QWidget* parent) : QDialog(parent)
     QPushButton* selectNoneButton = mButtonBox->addButton("Select None", QDialogButtonBox::ResetRole);
     connect(mButtonBox, &QDialogButtonBox::accepted, this, &QDialog::accept);
     connect(mButtonBox, &QDialogButtonBox::rejected, this, &QDialog::reject);
-    connect(selectAllButton, &QPushButton::clicked, [&](){ emit selectAllClicked(); });
-    connect(selectNoneButton, &QPushButton::clicked, [&](){ emit selectNoneClicked(); });
+    connect(selectAllButton, &QPushButton::clicked, this, [&](){ emit selectAllClicked(); });
+    connect(selectNoneButton, &QPushButton::clicked, this, [&](){ emit selectNoneClicked(); });
 
     // Setup dialog layout
     QVBoxLayout* mainLayout = new QVBoxLayout();
