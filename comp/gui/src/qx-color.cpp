126ffc38bbf9fdb70d339b79696e420a8652941a
Christian Heimlich <chris@pcserenity.com>
2022-03-17 05:38:32 -0400

    Add basic doc for qx-color

--- comp/gui/src/qx-color.cpp
+++ comp/gui/src/qx-color.cpp
@@ -8,11 +8,24 @@ namespace Qx
 // Color
 //===============================================================================================================
 
+/*!
+ *  @class Color
+ *
+ *  @brief The Color class is a collection of static functions pertaining to colors.
+ */
+
 //-Class Functions-----------------------------------------------------------------------------------------------
 //Public:
+
+/*!
+ *  Returns a pure black or pure white, whichever is more ideal for maximum visibility when displayed on top of
+ *  @a bgColor.
+ *
+ *  The choice is calculated according to version 2.0 of the WC3 Web Content Accessibility Guidelines.
+ */
 QColor Color::textFromBackground(QColor bgColor)
 {
-    // Based on W3 recommendations, using black & white text
+    // Based on W3C recommendations, using black & white text
     // See: https://www.w3.org/TR/WCAG20/ and https://www.w3.org/TR/WCAG20/#relativeluminancedef
     double contrastThreshold = 0.179;
     std::function<double(double)> componentFunc = [](double ch) { return ch < 0.03928 ? ch/12.92 : std::pow((ch+0.055)/1.055, 2.4); };
