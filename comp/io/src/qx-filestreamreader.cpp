0a29445ee9ce93295853e7df0e5a8f806c33ba12
Christian Heimlich <chris@pcserenity.com>
2022-03-18 05:48:34 -0400

    Add basic doc for qx-filestreamreader

--- comp/io/src/qx-filestreamreader.cpp
+++ comp/io/src/qx-filestreamreader.cpp
@@ -11,16 +11,52 @@ namespace Qx
 // FileStreamReader
 //===============================================================================================================
 
+/*!
+ *  @class FileStreamReader
+ *
+ *  @brief The FileStreamReader class is a specialized wrapper for QDataStream that narrows and simplifies its
+ *  usage for reading files.
+ *
+ *  Most member functions are the same or slightly modified versions of those from QDataStream.
+ */
+
 //-Constructor---------------------------------------------------------------------------------------------------
 //Public:
+/*!
+ *  Constructs a file stream reader that is linked to @a file.
+ */
 FileStreamReader::FileStreamReader(QFile* file) : mSourceFile(file) {}
 
 //-Instance Functions--------------------------------------------------------------------------------------------
 //Public:
+/*!
+ *  Returns @c true if the reader has reached the end of the file; otherwise returns @c false
+ */
 bool FileStreamReader::atEnd() const { return mStreamReader.atEnd(); }
+
+/*!
+ *  Returns the current byte order setting.
+ *
+ *  @sa setByteOrder().
+ */
 QDataStream::ByteOrder FileStreamReader::byteOrder() const { return mStreamReader.byteOrder(); }
+
+/*!
+ *  Returns the floating point precision of the file stream reader.
+ *
+ *  @sa setFloatingPointPrecision().
+ */
 QDataStream::FloatingPointPrecision FileStreamReader::floatingPointPrecision() const { return mStreamReader.floatingPointPrecision(); }
 
+/*!
+ *  Reads @a len bytes from the stream into @a data and returns an operation report.
+ *
+ *  @a data is automatically allocated. The data is @e not decoded.
+ *
+ *  @note Unlike with a raw QDataStream, if the number of bytes actually read is less than @a len it is treated as a
+ *  QDataStream::ReadPastEnd error since the length of a QFile based I/O device should always be known and data is not
+ *  received in chunks for that type.
+ */
 IoOpReport FileStreamReader::readRawData(QByteArray& data, int len)
 {
     // Allocate buffer
@@ -44,10 +80,48 @@ IoOpReport FileStreamReader::readRawData(QByteArray& data, int len)
         return IoOpReport(IO_OP_READ, IO_SUCCESS, *mSourceFile);
 }
 
+/*!
+ *  Resets the status of the file stream reader.
+ *
+ *  @sa QDataStream::Status, and status().
+ */
 void FileStreamReader::resetStatus() { mStreamReader.resetStatus(); }
+
+/*!
+ *  Sets the serialization byte order to @a bo.
+ *
+ *  The @a bo parameter can be QDataStream::BigEndian or QDataStream::LittleEndian.
+ *
+ *  The default setting is big endian.
+ *
+ *  @sa byteOrder().
+ */
 void FileStreamReader::setByteOrder(QDataStream::ByteOrder bo) { mStreamReader.setByteOrder(bo); }
+
+/*!
+ *  Sets the floating point precision of the file stream reader to @a precision.
+ *
+ *  All floating point numbers will be read using the stream's precision regardless of the stream
+ *  operator called.
+ *
+ *  The @a bo parameter can be QDataStream::BigEndian or QDataStream::LittleEndian.
+ *
+ *  @note This property does not affect the deserialization of qfloat16 instances.
+ *  The default setting is big endian.
+ *
+ *  @sa byteOrder().
+ */
 void FileStreamReader::setFloatingPointPrecision(QDataStream::FloatingPointPrecision precision) { mStreamReader.setFloatingPointPrecision(precision); }
 
+/*!
+ *  Skips @a len bytes from the file and returns an operation report.
+ *
+ *  This is equivalent to calling readRawData() on a buffer of length len and ignoring the buffer.
+ *
+ *  @note Unlike with a raw QDataStream, if the number of bytes actually skipped is less than @a len it is treated as a
+ *  QDataStream::ReadPastEnd error since the length of a QFile based I/O device should always be known and data is not
+ *  received in chunks for that type.
+ */
 IoOpReport FileStreamReader::skipRawData(int len)
 {
     // Skip data,
@@ -68,13 +142,27 @@ IoOpReport FileStreamReader::skipRawData(int len)
         return IoOpReport(IO_OP_READ, IO_SUCCESS, *mSourceFile);
 }
 
+/*!
+ *  Returns the status of the file stream reader.
+ *
+ *  @sa QDataStream::Status
+ */
 IoOpReport FileStreamReader::status()
 {
     return IoOpReport(IoOpType::IO_OP_READ, DATA_STRM_STAT_MAP.value(mStreamReader.status()), *mSourceFile);
 }
 
+/*!
+ *  Returns the file associated with the file stream reader.
+ */
 QFile* FileStreamReader::file() { return mSourceFile; }
 
+/*!
+ *  Opens the file associated with the file stream reader and returns an operation report.
+ *
+ *  This function must be called before any data is read, unless the file already open
+ *  in a mode that supports reading before the stream was constructed.
+ */
 IoOpReport FileStreamReader::openFile()
 {
     // Check file
@@ -95,6 +183,12 @@ IoOpReport FileStreamReader::openFile()
     return IoOpReport(IO_OP_WRITE, IO_SUCCESS, *mSourceFile);
 }
 
+/*!
+ *  Closes the file associated with the file stream reader.
+ *
+ *  This function should be called when the stream is no longer needed, unless the file should
+ *  remain open for use elsewhere.
+ */
 void FileStreamReader::closeFile() { mSourceFile->close(); }
 
 }
