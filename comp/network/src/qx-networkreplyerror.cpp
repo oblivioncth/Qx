5ac62ebd9f393d4b5e2d703192c221cd91e064bf
Christian Heimlich <chris@pcserenity.com>
2022-03-19 06:27:36 -0400

    Add basic doc for qx-networkreplyerror

--- comp/network/src/qx-networkreplyerror.cpp
+++ comp/network/src/qx-networkreplyerror.cpp
@@ -8,13 +8,26 @@ namespace Qx
 // NetworkReplyError
 //===============================================================================================================
 
+/*!
+ *  @class NetworkReplyError
+ *
+ *  @brief The NetworkReplyError class provides a full error object for QNetworkReply, similar to other Qt
+ *  classes, that can be more convenient for processing errors than just QNetworkReply::NetworkError.
+ */
+
 //-Constructor---------------------------------------------------------------------------------------------------
 //Public:
+/*!
+ *  Constructs an invalid network reply error that is equivalent to QNetworkReply::NetworkError::NoError.
+ */
 NetworkReplyError::NetworkReplyError() :
     mErrorType(QNetworkReply::NoError),
     mUrl(QUrl()), mErrorText(QString())
 {}
 
+/*!
+ *  Constructs a network reply error from @a reply and @a url.
+ */
 NetworkReplyError::NetworkReplyError(QNetworkReply* reply, QUrl url) :
     mErrorType(reply->error()),
     mUrl(url), mErrorText(reply->errorString())
@@ -22,9 +35,26 @@ NetworkReplyError::NetworkReplyError(QNetworkReply* reply, QUrl url) :
 
 //-Instance Functions--------------------------------------------------------------------------------------------
 //Public:
+/*!
+ *  Returns @c true if the error is valid; otherwise returns @c false.
+ *
+ *  A network reply error is valid if its underlying type isn't QNetworkReply::NetworkError::NoError.
+ */
 bool NetworkReplyError::isValid() { return mErrorType != QNetworkReply::NetworkError::NoError; }
+
+/*!
+ *  Returns the error's underlying type.
+ */
 QNetworkReply::NetworkError NetworkReplyError::type() { return mErrorType; }
+
+/*!
+ *  Returns the URL that the error pertains to.
+ */
 QUrl NetworkReplyError::url() { return mUrl; }
+
+/*!
+ *  Returns the textual representation of the error.
+ */
 QString NetworkReplyError::text() { return mErrorText; }
 
 }
