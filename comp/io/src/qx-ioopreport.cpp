f014789ea2ec4e857e6b28df1c12388f5e6e1c50
Christian Heimlich <chris@pcserenity.com>
2022-03-19 03:57:50 -0400

    Add basic doc for qx-ioopreport

--- comp/io/src/qx-ioopreport.cpp
+++ comp/io/src/qx-ioopreport.cpp
@@ -4,12 +4,189 @@
 namespace Qx
 {
 
+/*!
+ *  @enum IoOpType
+ *
+ *  This defines the type of an IO operation.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_OP_READ
+ *  Describes a read operation.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_OP_WRITE
+ *  Describes a write operation.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_OP_ENUMERATE
+ *  Describes an enumeration operation (e.g. counting files in a directory)
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_OP_INSPECT
+ *  Describes an inspection operation (e.g. querying file permissions)
+ */
+
+/*!
+ *  @enum IoOpResultType
+ *
+ *  This defines type of an IO operation result.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_SUCCESS
+ *  The operation was performed successfully.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_UNKNOWN
+ *  An unknown error occurred.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_ACCESS_DENIED
+ *  Access to the resource was denied.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_NOT_A_FILE
+ *  The operation target exists, but is not a file as expected.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_NOT_A_DIR
+ *  The operation target exists, but is not a directory as expected.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_OUT_OF_RES
+ *  The operation failed from a lack of system resources.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_READ
+ *  A general read error occurred.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_WRITE
+ *  A general write error occurred.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_FATAL
+ *  A fatal error occurred.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_OPEN
+ *  The target resource could not be opened.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_ABORT
+ *  The operation was aborted.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_TIMEOUT
+ *  The operation timed out.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_REMOVE
+ *  The target resource could not be removed.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_RENAME
+ *  The target resource could not be renamed.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_REPOSITION
+ *  A form of seeking within a resource failed.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_RESIZE
+ *  The target resource could not be resized.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_COPY
+ *  The target resource could not be copied.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_FILE_DNE
+ *  The target file does not exist.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_DIR_DNE
+ *  The target directory does not exist.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_FILE_EXISTS
+ *  The file's destination is already occupied.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_CANT_MAKE_DIR
+ *  A directory could not be created.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_FILE_SIZE_MISMATCH
+ *  The expected length of a file (or portion of a file) was different than expected.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_CURSOR_OOB
+ *  Access to an out-of-bounds position was attempted.
+ */
+
+/*!
+ *  @var IoOpType IoOpType::IO_ERR_FILE_NOT_OPEN
+ *  The operation could not be performed because the target file is not open.
+ */
+
+/*!
+ *  @enum IoOpTargetType
+ *
+ *  This defines the type of an IO operation target.
+ */
+
+/*!
+ *  @var IoOpType IoOpTargetType::IO_FILE
+ *  The target is a file.
+ */
+
+/*!
+ *  @var IoOpType IoOpTargetType::IO_DIR
+ *  The target is a directory.
+ */
+
 //===============================================================================================================
 // IoOpReport
 //===============================================================================================================
 
+/*!
+ *  @class IoOpReport
+ *
+ *  @brief The IoOpReport class is a container for details regarding the outcome of an IO operation.
+ */
+
 //-Constructor---------------------------------------------------------------------------------------------------
 //Public:
+/*!
+ *  Creates a null IO operation report.
+ */
 IoOpReport::IoOpReport() :
     mNull(true),
     mOperation(IO_OP_ENUMERATE),
@@ -18,6 +195,15 @@ IoOpReport::IoOpReport() :
     mTarget(QString())
 {}
 
+/*!
+ *  Creates an IO operation report for a file target.
+ *
+ *  @param op The type of operation
+ *  @param res The type of result.
+ *  @param tar The target file.
+ *
+ *  @note @a tar is only used for descriptive purposes and the reference is not kept
+ */
 IoOpReport::IoOpReport(IoOpType op, IoOpResultType res, const QFile& tar) :
     mNull(false),
     mOperation(op),
@@ -28,6 +214,15 @@ IoOpReport::IoOpReport(IoOpType op, IoOpResultType res, const QFile& tar) :
     parseOutcome();
 }
 
+/*!
+ *  Creates an IO operation report for a directory target.
+ *
+ *  @param op The type of operation
+ *  @param res The type of result.
+ *  @param tar The target directory.
+ *
+ *  @note @a tar is only used for descriptive purposes and the reference is not kept
+ */
 IoOpReport::IoOpReport(IoOpType op, IoOpResultType res, const QDir& tar) :
      mNull(false),
      mOperation(op),
@@ -41,13 +236,44 @@ IoOpReport::IoOpReport(IoOpType op, IoOpResultType res, const QDir& tar) :
 
 //-Instance Functions--------------------------------------------------------------------------------------------
 //Public:
+/*!
+ *  Returns the type of operation.
+ */
 IoOpType IoOpReport::operation() const { return mOperation; }
+
+/*!
+ *  Returns the operation's result.
+ */
 IoOpResultType IoOpReport::result() const { return mResult; }
+
+/*!
+ *  Returns the operation's target type.
+ */
 IoOpTargetType IoOpReport::resultTargetType() const { return mTargetType; }
+
+/*!
+ *  Returns the path to the operations target.
+ */
 QString IoOpReport::target() const { return mTarget; }
+
+/*!
+ *  Returns a string that describes the operation and its result.
+ */
 QString IoOpReport::outcome() const { return mOutcome; }
+
+/*!
+ *  Returns a string containing more details of the operation and its result.
+ */
 QString IoOpReport::outcomeInfo() const { return mOutcomeInfo; }
+
+/*!
+ *  Returns @c true if the operation was successful; otherwise returns @c false.
+ */
 bool IoOpReport::wasSuccessful() const { return mResult == IO_SUCCESS; }
+
+/*!
+ *  Returns @c true if the report is null; otherwise returns @c false.
+ */
 bool IoOpReport::isNull() const { return mNull; }
 
 //Private:
