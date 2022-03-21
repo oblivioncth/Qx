90ba18253cb738476a040d11adbfc2aafd7c64b9
Christian Heimlich <chris@pcserenity.com>
2022-03-21 09:10:48 -0400

    Add basic doc for qx-common-widgets

--- comp/widgets/src/qx-common-widgets.cpp
+++ comp/widgets/src/qx-common-widgets.cpp
@@ -1,9 +1,28 @@
 // Unit Includes
 #include "qx/widgets/qx-common-widgets.h"
 
+/*!
+ *  @file qx-common-widgets.h
+ *
+ *  @brief The qx-common-widgets header file provides various types, variables, and functions related to
+ *  Qt widgets.
+ */
+
 namespace Qx
 {
 //-Namespace Functions-------------------------------------------------------------------------------------------------
+
+/*!
+ *  Displays @a error using a QMessageBox.
+ *
+ *  @param error The error to display.
+ *  @param choices The different option buttons to display.
+ *  @param defChoice The default button that is selected.
+ *  @return The value of the selected button.
+ *
+ *  @note This function only works with QMessageBox::StandardButton, not with custom buttons.
+ *  @todo Provide an example picture of the resultant message box.
+ */
 int postError(GenericError error, QMessageBox::StandardButtons choices, QMessageBox::StandardButton defChoice)
 {
     // Determine icon
