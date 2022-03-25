f942c760d07dbe7d594ea865922a0792a45986f7
Christian Heimlich <chris@pcserenity.com>
2022-03-25 10:05:20 -0400

    Add basic doc qx-xmlstreamwritererror

--- comp/xml/src/qx-xmlstreamwritererror.cpp
+++ comp/xml/src/qx-xmlstreamwritererror.cpp
@@ -8,19 +8,42 @@ namespace Qx
 // XmlStreamWriterError
 //===============================================================================================================
 
+/*!
+ *  @class XmlStreamWriterError
+ *
+ *  @brief The XmlStreamWriterError class provides a full error object for QXmlStreamWriterError, similar to other
+ *  Qt classes. QXmlStreamWriter doesn't have an intrinsic error type, but this class can be useful for passing
+ *  contextual error information when writing XML data so that intent is more clear.
+ *
+ *  @sa XmlStreamReaderError
+ */
+
 //-Constructor---------------------------------------------------------------------------------------------------
 //Public:
+/*!
+ *  Constructs an invalid XML stream writer error.
+ */
 XmlStreamWriterError::XmlStreamWriterError() :
     mErrorText()
 {}
 
+/*!
+ *  Constructs a XML stream writer error from @a errorText.
+ */
 XmlStreamWriterError::XmlStreamWriterError(QString errorText) :
     mErrorText(errorText)
 {}
 
 //-Instance Functions--------------------------------------------------------------------------------------------
 //Public:
+/*!
+ *  Returns @c true if the error is valid; otherwise returns @c false.
+ */
 bool XmlStreamWriterError::isValid() { return !mErrorText.isNull(); }
+
+/*!
+ *  Returns error's text.
+ */
 QString XmlStreamWriterError::text() { return mErrorText; }
 
 }
