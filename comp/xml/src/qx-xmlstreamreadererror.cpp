4c1869241ff3671632f94200772c796338c315ad
Christian Heimlich <chris@pcserenity.com>
2022-03-25 09:42:36 -0400

    Add basic doc for qx-xmlstreamreadererror

--- comp/xml/src/qx-xmlstreamreadererror.cpp
+++ comp/xml/src/qx-xmlstreamreadererror.cpp
@@ -8,18 +8,38 @@ namespace Qx
 // XmlStreamReaderError
 //===============================================================================================================
 
+/*!
+ *  @class XmlStreamReaderError
+ *
+ *  @brief The XmlStreamReaderError class provides a full error object for QXmlStreamReader, similar to other Qt
+ *  classes, that can be more convenient for processing errors than just QXmlStreamReader::Error.
+ *
+ *  @sa XmlStreamWriterError
+ */
+
 //-Constructor---------------------------------------------------------------------------------------------------
 //Public:
+/*!
+ *  Constructs an invalid XML stream reader error that is equivalent to QXmlStreamReader::Error::NoError.
+ */
 XmlStreamReaderError::XmlStreamReaderError() :
     mErrorType(QXmlStreamReader::NoError),
     mErrorText(STD_ERR_TXT.value(QXmlStreamReader::NoError))
 {}
 
+/*!
+ *  Constructs a XML stream reader error from @a standardError.
+ */
 XmlStreamReaderError::XmlStreamReaderError(QXmlStreamReader::Error standardError) :
     mErrorType(standardError),
     mErrorText(STD_ERR_TXT.value(standardError))
 {}
 
+/*!
+ *  Constructs a custom XML stream reader error from the string @a customError.
+ *
+ *  This sets the error type to QXmlStreamReader::Error::CustomError.
+ */
 XmlStreamReaderError::XmlStreamReaderError(QString customError) :
     mErrorType(QXmlStreamReader::Error::CustomError),
     mErrorText(customError)
@@ -27,8 +47,21 @@ XmlStreamReaderError::XmlStreamReaderError(QString customError) :
 
 //-Instance Functions--------------------------------------------------------------------------------------------
 //Public:
+/*!
+ *  Returns @c true if the error is valid; otherwise returns @c false.
+ *
+ *  A xml stream reader error is valid if its underlying type isn't QXmlStreamReader::Error::NoError.
+ */
 bool XmlStreamReaderError::isValid() { return mErrorType != QXmlStreamReader::Error::NoError; }
+
+/*!
+ *  Returns the error's underlying type.
+ */
 QXmlStreamReader::Error XmlStreamReaderError::type() { return mErrorType; }
+
+/*!
+ *  Returns the textual representation of the error.
+ */
 QString XmlStreamReaderError::text() { return mErrorText; }
 
 }
