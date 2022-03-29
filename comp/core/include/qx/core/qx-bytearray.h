a825ac8db2ca53053d29da5de2e82b5348290c7b
Christian Heimlich <chris@pcserenity.com>
2022-03-29 11:47:20 -0400

    Massively simplify ByteArray::toPrimitive

--- comp/core/include/qx/core/qx-bytearray.h
+++ comp/core/include/qx/core/qx-bytearray.h
@@ -39,59 +39,20 @@ public:
     }
 
     template<typename T>
-        requires fundamental<T>
+        requires arithmetic<T>
     static T toPrimitive(QByteArray ba, QSysInfo::Endian endianness = QSysInfo::ByteOrder)
     {
-        static_assert(std::numeric_limits<float>::is_iec559, "Only supports IEC 559 (IEEE 754) float"); // For floats
-        assert((ba.size() >= 2 && ba.size() <= 8 && isEven(ba.size())) || ba.size() == 1);
-
-        if(sizeof(T) == 1)
+        if(endianness == QSysInfo::LittleEndian)
         {
-            quint8 temp;
-
-            if(endianness == QSysInfo::BigEndian)
-                 temp = qFromBigEndian<quint8>(ba);
-            else if(endianness == QSysInfo::LittleEndian)
-                 temp = qFromLittleEndian<quint8>(ba);
-
-            T* out = reinterpret_cast<T*>(&temp);
-            return(*out);
+            if(ba.size() < sizeof(T))
+                ba.append(sizeof(T) - ba.size(),'\x00');
+            return qFromLittleEndian<T>(ba);
         }
-        else if(sizeof(T) == 2)
+        else
         {
-            quint16 temp;
-
-            if(endianness == QSysInfo::BigEndian)
-                 temp = qFromBigEndian<quint16>(ba);
-            else if(endianness == QSysInfo::LittleEndian)
-                 temp = qFromLittleEndian<quint16>(ba);
-
-            T* out = reinterpret_cast<T*>(&temp);
-            return(*out);
-        }
-        else if(sizeof(T) == 4)
-        {
-            quint32 temp;
-
-            if(endianness ==QSysInfo::BigEndian)
-                 temp = qFromBigEndian<quint32>(ba);
-            else if(endianness == QSysInfo::LittleEndian)
-                 temp = qFromLittleEndian<quint32>(ba);
-
-            T* out = reinterpret_cast<T*>(&temp);
-            return(*out);
-        }
-        else if(sizeof(T) == 8)
-        {
-            quint64 temp;
-
-            if(endianness == QSysInfo::BigEndian)
-                 temp = qFromBigEndian<quint64>(ba);
-            else if(endianness == QSysInfo::LittleEndian)
-                 temp = qFromLittleEndian<quint64>(ba);
-
-            T* out = reinterpret_cast<T*>(&temp);
-            return(*out);
+            if(ba.size() < sizeof(T))
+                ba.prepend(sizeof(T) - ba.size(),'\x00');
+            return qFromBigEndian<T>(ba);
         }
     }
 };
