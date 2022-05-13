7d3c697d0c3c227d293120c5a06114a5f6ae4b61
Christian <chris@pcserenity.com>
2022-05-12 20:34:07 -0400

    Use linux distro name, falling back to "Linux" where applicable

--- cmake/module/Utility.cmake
+++ cmake/module/Utility.cmake
@@ -30,23 +30,25 @@ endfunction()
 
 function(get_proper_system_name return)
     if(CMAKE_SYSTEM_NAME STREQUAL Windows)
-        set(${return} Windows)
+        set(${return} Windows PARENT_SCOPE)
     elseif(CMAKE_SYSTEM_NAME STREQUAL Linux)
-        # Get name
+        # Get distro name
         execute_process(
-            COMMAND sh -c "( lsb_release -ds || cat /etc/*release || uname -om ) 2>/dev/null | head -n1"
+            COMMAND sh -c "( awk -F= '\$1==\"NAME\" { print \$2 ;}' /etc/os-release || lsb_release -is ) 2>/dev/null"
             ERROR_QUIET
             RESULT_VARIABLE res
             OUTPUT_VARIABLE distro_name
             OUTPUT_STRIP_TRAILING_WHITESPACE
         )
 
-        if(NOT ${distro_name})
+        # Handle cleanup and fallback
+        if(("${distro_name}" STREQUAL ""))
             message(WARNING "Could not determine distro name. Falling back to 'Linux'")
             set(distro_name "Linux")
+        else()
+            string(REPLACE "\"" "" distro_name ${distro_name}) # Remove possible quotation
         endif()
 
-        set(${return} ${distro_name})
+        set(${return} "${distro_name}" PARENT_SCOPE)
     endif()
-
 endfunction()
