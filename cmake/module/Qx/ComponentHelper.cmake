fac621570c2d3817582a93037e705178f1957927
Christian Heimlich <chris@pcserenity.com>
2022-06-06 10:33:38 -0400

    Move cmake modules to sub-folder to prevent clashes in consumers

--- cmake/module/Qx/ComponentHelper.cmake
+++ cmake/module/Qx/ComponentHelper.cmake
@@ -1,7 +1,7 @@
 macro(register_qx_component)
     #================= Setup ==========================
     # Utility functions
-    include(Utility)
+    include(Qx/Utility)
 
     # Determine component name via folder name
     get_filename_component(COMPONENT_NAME_LC "${CMAKE_CURRENT_SOURCE_DIR}" NAME)
