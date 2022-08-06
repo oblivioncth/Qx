68f4213c77a77af7c7511bff13df11c6fe929139
Christian Heimlich <chris@pcserenity.com>
2022-08-06 01:37:17 -0400

    Add SetOnce<T>, container that only allows setting its value once

---
+++ comp/core/include/qx/core/qx-setonce.h
@@ -0,0 +1,43 @@
+#ifndef QX_SETONCE_H
+#define QX_SETONCE_H
+
+// Standard Library Includes
+#include <type_traits>
+
+namespace Qx
+{
+
+template<typename T>
+    requires std::is_trivially_assignable_v<T&, T>
+class SetOnce
+{
+//-Instance Members----------------------------------------------------------------------------------------------------
+private:
+    bool mSet;
+    T mValue;
+
+//-Constructor-------------------------------------------------------------------------------------------------------
+public:
+    SetOnce(T initial) :
+        mSet(false),
+        mValue(initial)
+    {}
+
+//-Instance Functions--------------------------------------------------------------------------------------------------
+public:
+    bool isSet() const { return mSet; }
+    const T& value() const { return mValue; }
+
+    SetOnce<T>& operator=(const T& value)
+    {
+        if(!mSet && mValue != value)
+        {
+            mValue = value;
+            mSet = true;
+        }
+    }
+};
+
+}
+
+#endif // QX_SETONCE_H
