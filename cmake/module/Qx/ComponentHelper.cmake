e4918f4e623ddbdbc1038ea6696fbaa40ff313bf
Christian Heimlich <chris@pcserenity.com>
2022-09-02 06:47:41 -0400

    Remove old HEADER_INSTALL_SUFFIX
    Held the value 'include' so that this could be changed if ever desired,
    but the name is so standard for public headers that this will basically
    never happen. The variable was also only used once, and therefore
    basically pointless anyway.

--- cmake/module/Qx/ComponentHelper.cmake
+++ cmake/module/Qx/ComponentHelper.cmake
@@ -173,7 +173,7 @@ macro(register_qx_component)
     )
     install(FILES "${CMAKE_CURRENT_BINARY_DIR}/include/${PROJECT_NAME_LC}/${COMPONENT_NAME_LC}.h"
         COMPONENT ${COMPONENT_TARGET_NAME}
-        DESTINATION "${HEADER_INSTALL_SUFFIX}/${COMPONENT_NAME_LC}/${PROJECT_NAME_LC}"
+        DESTINATION "include/${COMPONENT_NAME_LC}/${PROJECT_NAME_LC}"
         ${SUB_PROJ_EXCLUDE_FROM_ALL} # "EXCLUDE_FROM_ALL" if project is not top-level
     )
 
