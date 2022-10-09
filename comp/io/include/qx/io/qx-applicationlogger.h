79ea56c833c8f77faa839008b4ee042d1149bf09
Christian Heimlich <chris@pcserenity.com>
2022-10-09 00:40:52 -0400

    Add ApplicationLogger, for easily creating application logs

---
+++ comp/io/include/qx/io/qx-applicationlogger.h
@@ -0,0 +1,84 @@
+#ifndef APPLICATIONLOGGER_H
+#define APPLICATIONLOGGER_H
+
+// Qt Includes
+#include <QString>
+#include <QStringList>
+#include <QRegularExpression>
+
+// Qx Includes
+#include <qx/core/qx-genericerror.h>
+#include <qx/io/qx-textstreamwriter.h>
+
+namespace Qx
+{
+
+class ApplicationLogger
+{
+//-Class Variables----------------------------------------------------------------------------------------------------
+private:
+    static inline const QString HEADER_TEMPLATE = "[ %1 Execution Log ] (%2) : %3";
+    // NOTE: Changes to the members of this class might require changes to this pattern
+    static inline const QRegularExpression HEADER_PATTERN = QRegularExpression(R"(^\[ .* Execution Log \] \(.*\) : .+)",
+                                                                               QRegularExpression::MultilineOption);
+    static inline const QString EVENT_TEMPLATE = " - <%1> [%2] %3";
+    static inline const QString COMMANDLINE_LABEL = "Arguments:";
+    static inline const QString EVENTS_LABEL = "Events:";
+    static inline const QString FINISH_TEMPLATE = "---------- Execution finished %1 (Code %2) ----------";
+    static inline const QString FINISH_SUCCESS = "successfully";
+    static inline const QString FINISH_ERR = "prematurely";
+
+    static inline const QHash<GenericError::ErrorLevel, QString> ERROR_LEVEL_STR_MAP = {
+        {GenericError::Warning, "WARNING"},
+        {GenericError::Error, "ERROR"},
+        {GenericError::Critical, "CRITICAL"}
+    };
+
+//-Instance Variables-------------------------------------------------------------------------------------------------
+private:
+    // Setup
+    QString mFilePath;
+    QString mAppName;
+    QString mAppVersion;
+    QString mAppArguments;
+    QDateTime mConstructionTimeStamp;
+    int mMaxEntries;
+
+    // Working var
+    TextStreamWriter mTextStreamWriter;
+    IoOpReport mErrorStatus;
+
+//-Constructor--------------------------------------------------------------------------------------------------------
+public:
+    ApplicationLogger();
+    ApplicationLogger(const QString& filePath);
+    ApplicationLogger(const QString& filePath, const QCoreApplication* app);
+
+//-Instance Functions-------------------------------------------------------------------------------------------------
+public:
+    // Configure
+    QString filePath() const;
+    QString applicationName() const;
+    QString applicationVersion() const;
+    QString applicationArguments() const;
+
+    void setFilePath(const QString path);
+    void setApplicationName(const QString name);
+    void setApplicationVersion(const QString version);
+    void setApplicationArguments(const QString args);
+    void setApplicationArguments(const QStringList args);
+
+    // Operate
+    IoOpReport openLog();
+    IoOpReport recordVerbatim(QString text);
+    IoOpReport recordErrorEvent(QString src, GenericError error);
+    IoOpReport recordGeneralEvent(QString src, QString event);
+    IoOpReport finish(int returnCode);
+
+    // Status
+    IoOpReport error();
+    bool hasError();
+};
+
+}
+#endif // APPLICATIONLOGGER_H
---
+++ comp/io/src/qx-applicationlogger.cpp
@@ -0,0 +1,204 @@
+// Unit Includes
+#include "qx/io/qx-applicationlogger.h"
+
+// Qt Includes
+#include <QCoreApplication>
+
+// TODO: Make this work with a QIODevice instead of just files. Would require a lot of general io component reform.
+
+namespace Qx
+{
+
+//===============================================================================================================
+// ApplicationLogger
+//===============================================================================================================
+
+/*!
+ *  @class ApplicationLogger qx/io/qx-applicationlogger.h
+ *  @ingroup qx-io
+ *
+ *  @brief The ApplicationLogger class acts as a convenient means of producing an execution log for an application.
+ *
+ *  Often it is useful for an applications to produce a log file that provides additional information about its
+ *  inner-workings in order to assist with debugging, optimization, or error resolution.
+ *
+ *  ApplicationLogger simplifies this process by providing a simple interface through which to record basic
+ *  information about an application and record events/errors with automatic timestamps.
+ *
+ *
+ *
+ *  Details of your application should be provided via the classes various setters.
+ */
+
+//-Constructor---------------------------------------------------------------------------------------------------
+//Public:
+/*!
+ *  Creates an application logger with no details set.
+ */
+ApplicationLogger::ApplicationLogger() :
+    mMaxEntries(100),
+    mConstructionTimeStamp(QDateTime::currentDateTime()),
+    mTextStreamWriter(WriteMode::Append,  WriteOption::CreatePath | WriteOption::Unbuffered),
+    mErrorStatus()
+{}
+
+ApplicationLogger::ApplicationLogger(const QString& filePath) :
+    ApplicationLogger()
+{
+    mFilePath = filePath;
+}
+
+ApplicationLogger::ApplicationLogger(const QString& filePath, const QCoreApplication* app) :
+    ApplicationLogger()
+{
+    mFilePath = filePath;
+    mAppName = app->applicationName();
+    mAppVersion = app->applicationVersion();
+    mAppArguments = app->arguments().join(' ');
+}
+
+//-Instance Functions--------------------------------------------------------------------------------------------
+//Public:
+QString ApplicationLogger::filePath() const { return mFilePath; }
+QString ApplicationLogger::applicationName() const { return mAppName; }
+QString ApplicationLogger::applicationVersion() const { return mAppVersion; }
+QString ApplicationLogger::applicationArguments() const { return mAppArguments; }
+
+void ApplicationLogger::setFilePath(const QString path) { mFilePath = path; }
+void ApplicationLogger::setApplicationName(const QString name) { mAppName = name; }
+void ApplicationLogger::setApplicationVersion(const QString version) { mAppVersion = version; }
+void ApplicationLogger::setApplicationArguments(const QString args) { mAppArguments = args; }
+void ApplicationLogger::setApplicationArguments(const QStringList args) { mAppArguments = args.join(' '); }
+
+IoOpReport ApplicationLogger::openLog()
+{
+    //-Prepare Log File--------------------------------------------------
+    QFile logFile(mFilePath);
+    QFileInfo logFileInfo(logFile);
+    IoOpReport logFileOpReport;
+
+    QString entryStart;
+
+    //-Handle Formatting For Existing Log---------------------------------
+    if(logFileInfo.exists() && logFileInfo.isFile() && !fileIsEmpty(logFile))
+    {
+        // Add spacer to start of new entry
+        entryStart.prepend('\n');
+
+        /* Load existing log (would be nice to not load entire log into memory, but because of limitations on how
+         * deletion works this would require searching/writing the file (a temporary one for the latter) in chunks,
+         * which is also meh.
+         */
+        QString existingLog;
+        logFileOpReport = readTextFromFile(existingLog, logFile);
+        if(logFileOpReport.isFailure())
+        {
+            mErrorStatus = logFileOpReport;
+            return logFileOpReport;
+        }
+
+        // Get entry count and locations
+        QList<qsizetype> entryStartOffsets;
+        for(const QRegularExpressionMatch& match : HEADER_PATTERN.globalMatch(existingLog))
+            entryStartOffsets += match.capturedStart();
+
+
+        // Purge oldest entries if current count is at or above limit
+        if(entryStartOffsets.count() >= mMaxEntries)
+        {
+            int firstToKeep = entryStartOffsets.count() - mMaxEntries + 1; // +1 to account for new entry
+            qsizetype newStart = entryStartOffsets.at(firstToKeep);
+            existingLog = existingLog.sliced(newStart, existingLog.size() - newStart);
+
+            logFileOpReport = writeStringToFile(logFile, existingLog);
+            if(logFileOpReport.isFailure())
+            {
+                mErrorStatus = logFileOpReport;
+                return logFileOpReport;
+            }
+        }
+    }
+
+    // Open log through stream
+    mTextStreamWriter.setFilePath(mFilePath);
+    logFileOpReport = mTextStreamWriter.openFile();
+    if(logFileOpReport.isFailure())
+    {
+        mErrorStatus = logFileOpReport;
+        return logFileOpReport;
+    }
+
+
+    //-Construct entry start---------------------
+    // Header
+    entryStart += HEADER_TEMPLATE.arg(mAppName, mAppVersion, QDateTime::currentDateTime().toString()) + "\n";
+
+    // Start parameters
+    entryStart += COMMANDLINE_LABEL + ' ' + mAppArguments + '\n';
+
+    // Events start
+    entryStart += EVENTS_LABEL + '\n';
+
+    // Write start of entry
+    logFileOpReport = mTextStreamWriter.writeText(entryStart);
+    return logFileOpReport;
+}
+
+IoOpReport ApplicationLogger::recordVerbatim(QString text)
+{
+    if(!mErrorStatus.isFailure())
+    {
+        mErrorStatus = mTextStreamWriter.writeLine(text);
+        return mErrorStatus;
+    }
+
+    return IoOpReport();
+}
+
+IoOpReport ApplicationLogger::recordErrorEvent(QString src, GenericError error)
+{
+    if(!mErrorStatus.isFailure())
+    {
+        QString errorString = EVENT_TEMPLATE.arg(QTime::currentTime().toString(), src, ERROR_LEVEL_STR_MAP.value(error.errorLevel()) + ") " + error.primaryInfo());
+        if(!error.secondaryInfo().isNull())
+            errorString += " " + error.secondaryInfo();
+        if(!error.detailedInfo().isNull())
+            errorString += "\n\t" + error.detailedInfo().replace("\n", "\n\t");
+
+        mErrorStatus = mTextStreamWriter.writeLine(errorString);
+        return mErrorStatus;
+    }
+
+    return IoOpReport();
+}
+
+IoOpReport ApplicationLogger::recordGeneralEvent(QString src, QString event)
+{
+    if(!mErrorStatus.isFailure())
+    {
+        mErrorStatus = mTextStreamWriter.writeLine(EVENT_TEMPLATE.arg(QTime::currentTime().toString(), src, event));
+        return mErrorStatus;
+    }
+
+    return IoOpReport();
+}
+
+IoOpReport ApplicationLogger::finish(int returnCode)
+{
+    if(!mErrorStatus.isFailure())
+    {
+        // Print exit code
+        mErrorStatus = mTextStreamWriter.writeLine(FINISH_TEMPLATE.arg(returnCode == 0 ? FINISH_SUCCESS : FINISH_ERR).arg(returnCode));
+
+        // Close log
+        mTextStreamWriter.closeFile();
+        return mErrorStatus;
+    }
+
+    return IoOpReport();
+}
+
+IoOpReport ApplicationLogger::error() { return mErrorStatus; }
+bool ApplicationLogger::hasError() { return mErrorStatus.isFailure(); }
+
+}
