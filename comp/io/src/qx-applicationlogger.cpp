2994824d67eaf9e4eed19d37abbd9c77b6c01b00
Christian Heimlich <chris@pcserenity.com>
2022-10-09 01:48:27 -0400

    Add ApplicationLogger documentation

--- comp/io/src/qx-applicationlogger.cpp
+++ comp/io/src/qx-applicationlogger.cpp
@@ -25,7 +25,7 @@ namespace Qx
  *  ApplicationLogger simplifies this process by providing a simple interface through which to record basic
  *  information about an application and record events/errors with automatic timestamps.
  *
- *
+ *  @snippet qx-applicationlogger.cpp 0
  *
  *  Details of your application should be provided via the classes various setters.
  */
@@ -33,7 +33,7 @@ namespace Qx
 //-Constructor---------------------------------------------------------------------------------------------------
 //Public:
 /*!
- *  Creates an application logger with no details set.
+ *  Creates an application logger with no file path or details set.
  */
 ApplicationLogger::ApplicationLogger() :
     mMaxEntries(100),
@@ -42,12 +42,25 @@ ApplicationLogger::ApplicationLogger() :
     mErrorStatus()
 {}
 
+/*!
+ *  Creates an application logger set to record to the file at @a filePath.
+ *
+ *  @sa setFilePath().
+ */
 ApplicationLogger::ApplicationLogger(const QString& filePath) :
     ApplicationLogger()
 {
     mFilePath = filePath;
 }
 
+/*!
+ *  Creates an application logger set to record to the file at @a filePath.
+ *
+ *  The details of the application (name, version, and arguments) are automatically set using
+ *  the application instance @a app.
+ *
+ *  @sa QCoreApplication::instance().
+ */
 ApplicationLogger::ApplicationLogger(const QString& filePath, const QCoreApplication* app) :
     ApplicationLogger()
 {
@@ -59,23 +72,102 @@ ApplicationLogger::ApplicationLogger(const QString& filePath, const QCoreApplica
 
 //-Instance Functions--------------------------------------------------------------------------------------------
 //Public:
+/*!
+ *  Returns the path of the log file that the logger is set to record to.
+ *
+ *  @sa setFilePath().
+ */
 QString ApplicationLogger::filePath() const { return mFilePath; }
+
+/*!
+ *  Returns the name of the application that will be recorded in the log.
+ *
+ *  @sa setApplicationName().
+ */
 QString ApplicationLogger::applicationName() const { return mAppName; }
+
+/*!
+ *  Returns the version of the application that will be recorded in the log.
+ *
+ *  @sa setApplicationVersion().
+ */
 QString ApplicationLogger::applicationVersion() const { return mAppVersion; }
+
+/*!
+ *  Returns the arguments string of the application that will be recorded in the log.
+ *
+ *  @sa setApplicationArguments().
+ */
 QString ApplicationLogger::applicationArguments() const { return mAppArguments; }
 
+/*!
+ *  Returns the maximum number of entries allowed in the log before the oldest entry is purged.
+ *
+ *  @sa setMaximumEntries().
+ */
 int ApplicationLogger::maximumEntries() const { return mMaxEntries; }
-void ApplicationLogger::setFilePath(const QString& path) { mFilePath = path; }
-void ApplicationLogger::setApplicationName(const QString name) { mAppName = name; }
-void ApplicationLogger::setApplicationVersion(const QString version) { mAppVersion = version; }
-void ApplicationLogger::setApplicationArguments(const QString args) { mAppArguments = args; }
-void ApplicationLogger::setApplicationArguments(const QStringList args) { mAppArguments = args.join(' '); }
 
+/*!
+ *  Sets the path of the log file that the logger will use for recording to @a path.
+ *
+ *  The file is appended to with a newline separating the new log entry from the previous entry.
+ *
+ *  @note This must be called before the log is opened.
+ *
+ *  @sa filePath(), and setMaximumEntries().
+ */
+void ApplicationLogger::setFilePath(const QString& path) { mFilePath = path; }
+
+/*!
+ *  Sets the name of the application that will be recorded in the log to @a name.
+ *
+ *  @note This must be called before the log is opened or else it will have no effect.
+ *
+ *  @sa applicationName().
+ */
 void ApplicationLogger::setApplicationName(const QString& name) { mAppName = name; }
+
+/*!
+ *  Sets the version of the application that will be recorded in the log @a version.
+ *
+ *  @note This must be called before the log is opened or else it will have no effect.
+ *
+ *  @sa applicationVersion().
+ */
 void ApplicationLogger::setApplicationVersion(const QString& version) { mAppVersion = version; }
+
+/*!
+ *  Sets the arguments string of the application that will be recorded in the log to @a args.
+ *
+ *  @note This must be called before the log is opened or else it will have no effect.
+ *
+ *  @sa applicationArguments().
+ */
 void ApplicationLogger::setApplicationArguments(const QString& args) { mAppArguments = args; }
+
+/*!
+ *  @overload
+ *
+ *  @a args are joined together into a single string using spaces as a separator.
+ *
+ *  @sa applicationArguments().
+ */
 void ApplicationLogger::setApplicationArguments(const QStringList& args) { mAppArguments = args.join(' '); }
+
+/*!
+ *  Sets the maximum number of entries to be kept in the log before the oldest one is purged.
+ *
+ *  @note This must be called before the log is opened or else it will have no effect.
+ *
+ *  @sa maximumEntries().
+ */
 void ApplicationLogger::setMaximumEntries(int max) { mMaxEntries = max; }
+
+/*!
+ *  Opens the log for recording and returns a report noting if the operation succeeded or failed.
+ *
+ *  @sa finish().
+ */
 IoOpReport ApplicationLogger::openLog()
 {
     //-Prepare Log File--------------------------------------------------
@@ -150,17 +242,27 @@ IoOpReport ApplicationLogger::openLog()
     return logFileOpReport;
 }
 
+/*!
+ *  Records @a text to the log directly as provided with no timestamp or list item separator and terminates it with a newline.
+ *
+ *  @sa recordGeneralEvent(), and recordErrorEvent().
+ */
 IoOpReport ApplicationLogger::recordVerbatim(QString text)
 {
     if(!mErrorStatus.isFailure())
-    {
         mErrorStatus = mTextStreamWriter.writeLine(text);
-        return mErrorStatus;
-    }
 
     return mErrorStatus;
 }
 
+/*!
+ *  Records @a error to the log with a timestamp and label that denotes the error's severity.
+ *
+ *  The parameter @a src acts as a means to identify which section of the application the error originated from, whether
+ *  it be a source file name, class name, etc.
+ *
+ *  @sa recordGeneralEvent().
+ */
 IoOpReport ApplicationLogger::recordErrorEvent(QString src, GenericError error)
 {
     if(!mErrorStatus.isFailure())
@@ -172,40 +274,74 @@ IoOpReport ApplicationLogger::recordErrorEvent(QString src, GenericError error)
             errorString += "\n\t" + error.detailedInfo().replace("\n", "\n\t");
 
         mErrorStatus = mTextStreamWriter.writeLine(errorString);
-        return mErrorStatus;
     }
 
     return mErrorStatus;
 }
 
+/*!
+ *  Records @a event to the log with a timestamp.
+ *
+ *  The parameter @a src acts as a means to identify which section of the application the error originated from, whether
+ *  it be a source file name, class name, etc.
+ *
+ *  @sa recordErrorEvent().
+ */
 IoOpReport ApplicationLogger::recordGeneralEvent(QString src, QString event)
 {
     if(!mErrorStatus.isFailure())
-    {
         mErrorStatus = mTextStreamWriter.writeLine(EVENT_TEMPLATE.arg(QTime::currentTime().toString(), src, event));
-        return mErrorStatus;
-    }
 
     return mErrorStatus;
 }
 
+/*!
+ *  Writes a footer to the current log entry that notes the application's return code and whether or
+ *  not execution finished successfully, then closes the log.
+ *
+ *  If @a returnCode equals @c 0, execution is considered to have been successful; otherwise, it will
+ *  be considered to have ended prematurely.
+ *
+ *  @sa openLog().
+ */
 IoOpReport ApplicationLogger::finish(int returnCode)
 {
     if(!mErrorStatus.isFailure())
-    {
-        // Print exit code
         mErrorStatus = mTextStreamWriter.writeLine(FINISH_TEMPLATE.arg(returnCode == 0 ? FINISH_SUCCESS : FINISH_ERR).arg(returnCode));
 
     // Close log
     mTextStreamWriter.closeFile();
     return mErrorStatus;
-    }
-
-    return IoOpReport();
 }
 
+/*!
+ *  Returns the error status of the logger.
+ *
+ *  The status is a report of the last operation performed by ApplicationLogger. If no operation has
+ *  been performed since the logger was constructed or resetStatus() was last called the report will be null.
+ *
+ *  @sa resetStatus().
+ */
 IoOpReport ApplicationLogger::status() { return mErrorStatus; }
+
+/*!
+ *  Resets the status of the logger.
+ *
+ *  @note
+ *  If an error occurs while writing the logger will ignore all further write attempts and hold its
+ *  current status until this function is called.
+ *
+ *  @sa status().
+ */
 void ApplicationLogger::resetStatus() { mErrorStatus = IoOpReport(); }
+
+/*!
+ *  Returns @c true if the logger's current status indicates that an error has occurred; otherwise, returns @c false.
+ *
+ *  Equivalent to `status().isFailure()`.
+ *
+ *  @sa status().
+ */
 bool ApplicationLogger::hasError() { return mErrorStatus.isFailure(); }
 
 }
