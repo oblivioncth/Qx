8fc8c55fcb149d143433a41321ae4ade4ed58841
Christian Heimlich <chris@pcserenity.com>
2024-08-15 14:29:31 -0400

    Add qxFuncAggregate, useful for std::variant overload pattern

--- lib/utility/include/qx/utility/qx-helpers.h
+++ lib/utility/include/qx/utility/qx-helpers.h
@@ -4,6 +4,16 @@
 // Standard Library Includes
 #include <type_traits>
 
+//Non-namespace Structs----------------------------------------------------------
+/* TODO: Figure out how to constrain this to only accept functors, issue is at least as of C++20
+ * there doesnt seem to be a way to check if a type has an arbitrary number of operator() overloads
+ * with an arbitrary number of arguments.
+ */
+template<typename... Functors>
+struct qxFuncAggregate : Functors... {
+    using Functors::operator()...;
+};
+
 //Non-namespace Functions----------------------------------------------------------
 template <typename T>
 const T qxAsConst(T&& t) { return std::move(t); }
